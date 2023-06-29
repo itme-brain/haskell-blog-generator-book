6f72a7926b2ca6657f97e2b2d37af7c3a7fa2d46
Bryan Ramos <bryan@ramos.codes>
2023-06-29 09:56:50 -0400

    fixed

--- src/HsBlog/Markup.hs
+++ src/HsBlog/Markup.hs
@@ -21,30 +21,12 @@ data Structure
   | CodeBlock [String]
   deriving (Eq, Show)
 
-data Context
-  = CtxHeading Natural String
-  | CtxParagraph [String]
-  | CtxUnorderedList [String]
-  | CtxOrderedList [String]
-  | CtxCodeBlock [String]
-
-replicate' :: Int -> a -> [a]
-replicate' n x =
-  if n <= 0
-    then []
-    else x : replicate' (n - 1) x
-
-even' :: Int -> Bool
-even' n =
-  if n == 0
-    then True
-    else odd' (n - 1)
-
-odd' :: Int -> Bool
-odd' n =
-  if n == 0
-    then False
-    else even' (n - 1)
+-- data Context
+--   = CtxHeading Natural String
+--   | CtxParagraph [String]
+--   | CtxUnorderedList [String]
+--   | CtxOrderedList [String]
+--   | CtxCodeBlock [String]
 
 parse :: String -> Document
 parse = parseLines Nothing . lines -- (1)
